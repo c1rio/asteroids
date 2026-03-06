76c8f3a78d535b0a09bf481ae11b2035e01ac1d8
c1rio <c1riodev@gmail.com>
2026-03-06 10:22:02 -0300

    rotation function added

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ def main():
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
+        dt = clock.tick(60) / 1000
+        player.update(dt)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return    
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
